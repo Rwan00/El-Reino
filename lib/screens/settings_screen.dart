b044db63d44bdf4fe81054277163b60d604c7678
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-04-08 02:38:21 +0200

    profile_screen ui

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,50 +1,145 @@
+import 'package:el_reino/cubits/app_cubit/app_cubit.dart';
+import 'package:el_reino/cubits/app_cubit/app_state.dart';
+import 'package:el_reino/theme/fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../widgets/app_btn.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 190,
-            child: Stack(
-              alignment: AlignmentDirectional.bottomEnd,
-              children: [
-                Align(
-                  alignment: AlignmentDirectional.topCenter,
-                  child: Container(
-                    height: 140,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      image: const DecorationImage(
-                        image: NetworkImage(
-                          "https://i.pinimg.com/564x/66/fa/bc/66fabc32423171aa0fd850091b65d1e0.jpg",
+    return BlocConsumer<AppCubit, AppStates>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        var cubit = AppCubit.get(context);
+        return Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 190,
+                child: Stack(
+                  alignment: AlignmentDirectional.bottomStart,
+                  children: [
+                    Align(
+                      alignment: AlignmentDirectional.topCenter,
+                      child: Container(
+                        height: 140,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          image: const DecorationImage(
+                            image: NetworkImage(
+                              "https://i.pinimg.com/564x/66/fa/bc/66fabc32423171aa0fd850091b65d1e0.jpg",
+                            ),
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                        fit: BoxFit.cover,
                       ),
                     ),
-                  ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12.0),
+                      child: CircleAvatar(
+                        radius: 55,
+                        backgroundColor: Colors.white,
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(
+                            "https://i.pinimg.com/564x/97/f0/cb/97f0cb0bd91313be32a74ff14584d0f7.jpg",
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                const CircleAvatar(
-                  radius: 55,
-                  backgroundColor: Colors.white,
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundImage: NetworkImage(
-                      "https://i.pinimg.com/564x/97/f0/cb/97f0cb0bd91313be32a74ff14584d0f7.jpg",
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Rwan",
+                    style: heading,
+                  ),
+                  const Spacer(),
+                  AppBtn(label: "Edit Your Profile", onPressed: () {}),
+                ],
+              ),
+              Text(
+                "To Be or Not To Be..",
+                style: subTitle,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "100",
+                          style: titleStyle,
+                        ),
+                        Text(
+                          "Followers",
+                          style: subTitle,
+                        ),
+                      ],
                     ),
                   ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "100",
+                          style: titleStyle,
+                        ),
+                        Text(
+                          "Followers",
+                          style: subTitle,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "100",
+                          style: titleStyle,
+                        ),
+                        Text(
+                          "Followers",
+                          style: subTitle,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "100",
+                          style: titleStyle,
+                        ),
+                        Text(
+                          "Followers",
+                          style: subTitle,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
